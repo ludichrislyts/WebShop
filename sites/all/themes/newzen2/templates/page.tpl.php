8d2b23b1390b87bcbd04667b9d1b3fa9eff22680
micahcourey <student@epicodus.com>
2015-11-12 11:59:28 -0800

    Update newzen sub-theme

--- sites/all/themes/newzen2/templates/page.tpl.php
+++ sites/all/themes/newzen2/templates/page.tpl.php
@@ -30,6 +30,9 @@
       </div>
     <?php endif; ?>
 
+
+
+
     <?php print render($page['header']); ?>
 
   </header>
@@ -37,25 +40,6 @@
 
 
   <div id="main">
-
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div>
 
     <div id="navigation">
 
@@ -82,6 +66,25 @@
 
       <?php print render($page['navigation']); ?>
 
+    </div>
+
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
     </div>
 
     <?php
